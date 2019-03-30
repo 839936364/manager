3ddd78900a5989cdd4994554e23325d1c12437b6
839936364 <839936364@qq.com>
2019-03-30 15:19:22 +0800

    验

--- src/components/login.vue
+++ src/components/login.vue
@@ -5,15 +5,16 @@
       <!-- 标题 -->
       <h2>用户登录</h2>
       <!-- 饿了么ui的form -->
-      <el-form label-position="top" label-width="80px">
-        <el-form-item label="用户名" v-model="userName">
-          <el-input></el-input>
+      <el-form label-position="top" label-width="80px" :model="loginForm" :rules="rules" ref="loginForm">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="loginForm.userName"></el-input>
         </el-form-item>
-        <el-form-item label="密码" v-model="passWord">
-          <el-input></el-input>
+        <el-form-item label="密码" prop="passWord">
+          <el-input v-model="loginForm.passWord"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button class="my-btn" type="primary">登录</el-button>
+          <el-button type="primary" @click="submit('loginForm')">登录</el-button>
+          <el-button type="success" @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -24,11 +25,37 @@
 export default {
   data() {
     return {
-      userName: "",
-      passWord: ""
+      loginForm: {
+        userName: "",
+        passWord: ""
+      },
+      rules: {
+
+        userName: [
+          { required: true, message: "账号不能为空", trigger: "blur" },
+          { min: 6, max: 16, message: "长度在 6 到 16 个字符", trigger: "blur" }
+        ],
+        passWord: [
+          { required: true, message: "密码不能为空付出", trigger: "blur" },
+          { min: 6, max: 20, message: "长度在 6 到 20 个字符", trigger: "blur" }
+        ]
+      }
     };
   },
-  methods: {}
+  methods: {
+      submit(loginForm) {
+        this.$refs[loginForm].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            return false;
+          }
+        });
+      },
+      resetForm(loginForm) {
+        this.$refs[loginForm].resetFields();
+      }
+  }
 };
 </script>
 
