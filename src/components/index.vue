3069a1c6b6bd34b2a9123839a60f01cc9ceeeba3
839936364 <839936364@qq.com>
2019-03-30 22:51:02 +0800

    第一天结束

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,13 +1,74 @@
 <template>
-    <div>index</div>
+  <el-container class="index-container">
+    <!-- 头部 -->
+    <el-header class="index-header">
+      <el-row>
+        <el-col :span="2">
+          <img src="../assets/logo.png" alt>
+        </el-col>
+        <el-col :span="20">
+          <h2>不管这是个啥,反正写就对了</h2>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="success">退出</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <!-- 主体 -->
+    <el-container class="index-main">
+      <!-- 左边栏 -->
+      <el-aside width="200px" class="index-left">
+        <el-menu router default-active="2" class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="users">
+              <span class="el-icon-menu"></span> 选项1
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右边公共区域 -->
+      <el-main class="index-right">
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-
-}
+  name: "index"
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.index-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .index-header {
+    height: 60px;
+    background-color: #b3c0d1;
+    line-height: 60px;
+    h2 {
+      margin: 0;
+      padding: 0;
+      text-align: center;
+    }
+  }
+  .index-main {
+    flex: 1;
+    .index-left {
+      background-color: pink;
+    }
+    .index-right {
+      background-color: #e9eef3;
+      padding-top: 0;
+    }
+  }
+}
 </style>
+
--- src/router.js
+++ src/router.js
@@ -2,13 +2,27 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
-import index from "./components/index.vue";
 import login from "./components/login.vue";
+import index from "./components/index.vue";
+import users from "./components/users.vue";
 
 const router = new VueRouter({
   routes: [
-    { path: "/", component: index },
-    { path: "/login", component: login },
+    {
+      path: "/login",
+      component: login
+    },
+    {
+      path: "/",
+      component: index,
+      children: [
+        {
+          path: "users",
+          component: users
+        },
+        {path:'/', redirect: 'users'}
+      ]
+    }
   ]
 });
 export default router;
